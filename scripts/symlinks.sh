1a94408941f2a13be3773345c6e86e359b707f9c
navazjm <michaelnavs@gmail.com>
2025-02-23 15:29:08 -0600

    scripts/symlinks.sh: point files to this dir for configs

---
+++ scripts/symlinks.sh
@@ -0,0 +1,7 @@
+ln -s $HOME/.config/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/.config/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/.config/dotfiles/.yabairc $HOME/.yabairc
+ln -s $HOME/.config/dotfiles/.skhdrc $HOME/.skhdrc
+ln -s $HOME/.config/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/.config/dotfiles/alacritty $HOME/.config/alacritty 
+ln -s $HOME/.config/dotfiles/nvim $HOME/.config/nvim 

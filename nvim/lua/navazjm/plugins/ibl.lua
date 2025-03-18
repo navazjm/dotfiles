76b4a635b5c6eb81211050bc5b341bf5750f5803
navazjm <michaelnavs@gmail.com>
2025-03-17 23:09:33 -0500

    nvim: update ident blank line config

--- nvim/lua/navazjm/plugins/ibl.lua
+++ nvim/lua/navazjm/plugins/ibl.lua
@@ -1,7 +1,6 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    commit = "d343409a9f7ce4573955a181a0dfb71a103040d3",
     opts = {
         indent = {
             char = "┊", -- "│", "▎", "┊"

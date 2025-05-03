9f08786f10d4d90eb1aed4e577cc2b38d69af618
navazjm <michaelnavs@gmail.com>
2025-05-02 22:23:45 -0500

    nvim: transparent bg

--- nvim/lua/navazjm/plugins/colorschemes.lua
+++ nvim/lua/navazjm/plugins/colorschemes.lua
@@ -3,8 +3,17 @@ return {
         dir = "~/repos/ascua.nvim", -- Absolute or relative path to the plugin
         name = "ascua", -- Optional, to ensure a unique name
         dev = true,
-        opts = {},
-        config = function()
+        opts = {
+            bold_styles = false,
+            color_overrides = {},
+            group_overrides = {},
+            italic_comments = false,
+            underline_links = false,
+            terminal_colors = true,
+            transparent = true,
+        },
+        config = true,
+        init = function()
             vim.cmd("colorscheme ascua")
         end,
     },

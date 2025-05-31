ac7db7fa43a12142d0d774d626fddcc18026304f
navazjm <michaelnavs@gmail.com>
2025-05-31 10:29:27 -0500

    improvements from setting up voidlinux a second time

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,5 +1,25 @@
-ln -s $HOME/.config/dotfiles/.bashrc $HOME/.bashrc
-ln -s $HOME/.config/dotfiles/.tmux.conf $HOME/.tmux.conf
-ln -s $HOME/.config/dotfiles/.gitconfig $HOME/.gitconfig
-ln -s $HOME/.config/dotfiles/nvim $HOME/.config/nvim 
-ln -s $HOME/.config/dotfiles/awesome $HOME/.config/awesome 
+link_file() {
+    local source=$1
+    local dest=$2
+
+    if [ -z "$source" ] || [ -z "$dest" ]; then
+        echo "Usage: link_file <source> <dest>"
+        return 1
+    fi
+
+    if [ -e "$dest" ] || [ -L "$dest" ]; then
+        echo "Removing existing $dest"
+        rm -rf "$dest"
+    fi
+
+    ln -s "$source" "$dest"
+    echo "Created symlink: $dest -> $source"
+}
+
+link_file "$HOME/.config/dotfiles/.xinitrc" "$HOME/.xinitrc"
+link_file "$HOME/.config/dotfiles/.bashrc" "$HOME/.bashrc"
+link_file "$HOME/.config/dotfiles/.bash_profile" "$HOME/.bash_profile"
+link_file "$HOME/.config/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
+link_file "$HOME/.config/dotfiles/.gitconfig" "$HOME/.gitconfig"
+link_file "$HOME/.config/dotfiles/nvim" "$HOME/.config/nvim" 
+link_file "$HOME/.config/dotfiles/awesome" "$HOME/.config/awesome" 
